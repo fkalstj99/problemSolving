38df78bfb8673efbe90ef378d8a161241c9bd217
fkalstj99 <59814025+fkalstj99@users.noreply.github.com>
2020-08-13 00:46:36 +0900

    dp

--- DP/1463[1로만들기].py
+++ DP/1463[1로만들기].py
@@ -1,14 +1,16 @@
-n = int(input())
+N = int(input())
 
-dp = [0 for _ in range(n+1)]
+dp = [0]*(N+1)
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1  
+dp[0]= 0
+dp[1] = 0
 
-    if i%2 == 0 and dp[i] > dp[i//2] + 1 :
-        dp[i] = dp[i//2]+1
-        
-    if i%3 == 0 and dp[i] > dp[i//3] + 1 :
-        dp[i] = dp[i//3] + 1
-        
-print(dp[n])
+for i in range(2, N+1):
+  if i%2 == 0:
+    dp[i] = min(dp[i-1],dp[i//2]) +1
+  elif i%3 == 0:
+    dp[i] = min(dp[i-1], dp[i//3]) + 1
+  else:
+    dp[i] = dp[i-1] + 1
+
+print(dp[N])
---
+++ DP/9095[1,2,3더하기].py
@@ -0,0 +1,12 @@
+testCase = int(input())
+
+
+dp = [0, 1, 2, 4] + [0]*7
+
+for i in range(4,11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for _ in range(testCase):
+  N = int(input())
+  print(dp[N])
+
